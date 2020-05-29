2e14c61a215ef68daa1f42f4c78f2b94fd5244a3
Jiaojiao Fan <sbyebss@gmail.com>
2020-05-28 22:57:46 -0400

    prepare to check the change

---
+++ c++/build/CMakeFiles/Barycenters.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Barycenters.dir/src/main.cpp.o"
+  "Barycenters.pdb"
+  "Barycenters"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Barycenters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
